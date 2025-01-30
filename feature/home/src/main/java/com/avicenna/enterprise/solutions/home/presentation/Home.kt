ea858c47344c12152c5eac8ec5ed2e1fc54cc9db
usmanhakam0916 <usman.hakam@gmail.com>
2025-01-30 19:38:21 +0500

    Added movies card

--- feature/home/src/main/java/com/avicenna/enterprise/solutions/home/presentation/Home.kt
+++ feature/home/src/main/java/com/avicenna/enterprise/solutions/home/presentation/Home.kt
@@ -1,5 +1,8 @@
 package com.avicenna.enterprise.solutions.home.presentation
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -10,8 +13,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,43 +53,11 @@ internal fun HomeScreen(
         Column(
             modifier = Modifier.fillMaxWidth()
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .background(color = Color.Black)
-                    .padding(top = 4.dp, start = 4.dp, end = 4.dp)
-            ) {
-                Text(
-                    text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = Color.White,
-                    modifier = Modifier.weight(3f)
-                )
-                IconButton(onClick = {}) {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_nav_search),
-                        contentDescription = null,
-                        tint = Color.White,
-                            modifier = Modifier.size(26.dp)
-                        )
-                    }
-                    IconButton(onClick = {}) {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_watchlist),
-                            contentDescription = null,
-                            tint = Color.White,
-                            modifier = Modifier.size(26.dp)
-                        )
-                    }
-                    IconButton(onClick = {}) {
-                        Icon(
-                            painter = painterResource(R.drawable.fav_outline),
-                            contentDescription = null,
-                            tint = Color.White,
-                            modifier = Modifier.size(24.dp)
-                        )
-                    }
-                }
+            AppBar(
+                onSearchClick = {},
+                onFavoriteClick = {},
+                onWishlistClick = {}
+            )
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -171,8 +145,87 @@ internal fun HomeScreen(
                         )
                     }
                 }
+                Text(
+                    text = "Upcoming Movies",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.padding(top = 24.dp, start = 8.dp)
+                )
+
             }
         }
+    }
+}
+
+@Composable
+fun AppBar(
+    onSearchClick: () -> Unit,
+    onFavoriteClick: () -> Unit,
+    onWishlistClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .background(color = Color.Black)
+            .padding(top = 4.dp, start = 4.dp, end = 4.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.headlineSmall,
+            color = Color.White,
+            modifier = Modifier.weight(3f)
+        )
+        IconButton(onClick = onSearchClick) {
+            Icon(
+                painter = painterResource(R.drawable.ic_nav_search),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.size(26.dp)
+            )
+        }
+        IconButton(onClick = onWishlistClick) {
+            Icon(
+                painter = painterResource(R.drawable.ic_watchlist),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.size(26.dp)
+            )
+        }
+        IconButton(onClick = onFavoriteClick) {
+            Icon(
+                painter = painterResource(R.drawable.fav_outline),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun MovieCard(
+    @StringRes rating: Int,
+    @DrawableRes image: Int,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.width(50.dp).height(100.dp),
+        border = BorderStroke(1.dp,Color.White)
+    ) {
+        Text(
+            text = stringResource(rating),
+            fontSize = 12,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Image(
+            painter = painterResource(image),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
 
